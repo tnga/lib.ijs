b93b322830f01afedaaa04622a0bf0f498414bf6
tnga <devtnga@gmail.com>
2015-07-20 09:26:40 +0100

    use of Brackets-beautify

--- i.js
+++ i.js
@@ -8,65 +8,65 @@
  * @license: GPL v3.0
  * @author : Tindo Ngoufo Arsel
  * @version: 0.0_(15.04) 
-*/
+ */
 
 var iJS = {
 
-    isNumber : function( arg ){
+    isNumber: function (arg) {
 
-        if( (typeof arg).toLowerCase() === "number" || arg instanceof Number )
-            return true ;
+        if ((typeof arg).toLowerCase() === "number" || arg instanceof Number)
+            return true;
         else
-            return false ;
-    } ,
+            return false;
+    },
 
-    isBoolean : function( arg ){
+    isBoolean: function (arg) {
 
-        if( (typeof arg).toLowerCase() === "boolean" || arg instanceof Boolean )
-            return true ;
+        if ((typeof arg).toLowerCase() === "boolean" || arg instanceof Boolean)
+            return true;
         else
-            return false ;
-    } ,
+            return false;
+    },
 
-    isString : function( arg ){
+    isString: function (arg) {
 
-        if( (typeof arg).toLowerCase() === "string" || arg instanceof String )
-            return true ;
+        if ((typeof arg).toLowerCase() === "string" || arg instanceof String)
+            return true;
         else
-            return false ;
-    } ,
+            return false;
+    },
 
-    isArray : function( arg ){
+    isArray: function (arg) {
 
-        if( arg instanceof Array )
-            return true ;
+        if (arg instanceof Array)
+            return true;
         else
-            return false ;
-    } ,
+            return false;
+    },
 
-    isHTMLElement : function( arg ){
+    isHTMLElement: function (arg) {
 
-        if( arg instanceof HTMLElement )
-            return true ;
+        if (arg instanceof HTMLElement)
+            return true;
         else
-            return false ;
-    } ,
+            return false;
+    },
 
-    isHTMLImageElement : function( arg ){
+    isHTMLImageElement: function (arg) {
 
-        if( arg instanceof HTMLImageElement )
-            return true ;
+        if (arg instanceof HTMLImageElement)
+            return true;
         else
-            return false ;
-    } ,
+            return false;
+    },
 
-    isObject : function( arg ){
+    isObject: function (arg) {
 
-        if( (typeof arg).toLowerCase() === "object" )
-            return true ;
+        if ((typeof arg).toLowerCase() === "object")
+            return true;
         else
-            return false ;
-    } ,
+            return false;
+    },
 
     /* permet de changer les images pour l'animations des chargements (simuler un "loader")
 	 *
@@ -77,98 +77,90 @@ var iJS = {
 	 doivent être de la forme 'imglaod0.png', 'imglaod1.png', ... où 'imgload' est ici le nom globlal
 	*"imgFormat" est le format des images à animer. Par défaut (ou s'il n'est pas renseigner) c'est 'png'.
 	*/
-    mi_loader : function(imgContainer, imgDir, imgLength, imgGlobalName, imgFormat ){
+    mi_loader: function (imgContainer, imgDir, imgLength, imgGlobalName, imgFormat) {
 
-        if( iJS.isString( imgDir ) )
-            this.imgDir = imgDir ;
-        if( iJS.isNumber( imgLength ) )
-            this.imgLength = imgLength ;
-        if( iJS.isString( imgGlobalName ) )
-            this.imgGlobalName = imgGlobalName ;
+        if (iJS.isString(imgDir))
+            this.imgDir = imgDir;
+        if (iJS.isNumber(imgLength))
+            this.imgLength = imgLength;
+        if (iJS.isString(imgGlobalName))
+            this.imgGlobalName = imgGlobalName;
 
-        this.imgFormat = ( iJS.isString(imgFormat) ) ? imgFormat : "png" ;
+        this.imgFormat = (iJS.isString(imgFormat)) ? imgFormat : "png";
 
-        if( iJS.isString( imgContainer ) )
-            if( iJS.isHTMLImageElement( document.getElementById( imgContainer ) ) )
-                this.imgContainer = document.getElementById( imgContainer ) ;
-            else if( iJS.isHTMLImageElement( imgContainer ) )
-                this.imgContainer = imgContainer ;
+        if (iJS.isString(imgContainer))
+            if (iJS.isHTMLImageElement(document.getElementById(imgContainer)))
+                this.imgContainer = document.getElementById(imgContainer);
+            else if (iJS.isHTMLImageElement(imgContainer))
+            this.imgContainer = imgContainer;
 
-        this.imgIndex = 0 ; //représente le numéro de l'image à afficher
-        this.imgPath = "" ; //représente le chemin vers l'image à afficher
-        this.loaderID =0 ; //puisque clearInterval pourra être utilisées
+        this.imgIndex = 0; //représente le numéro de l'image à afficher
+        this.imgPath = ""; //représente le chemin vers l'image à afficher
+        this.loaderID = 0; //puisque clearInterval pourra être utilisées
 
         /*la fonction ci-dessous permet de changer ou remplacer l'image affichée par la suivante.
-			 *le paramètre "loader" n'est nécessaire lorsque la fonction est passer en paramètre à une autre. 
-			 *en effet il fera le plus souvent référence à l'objet lui même (au "this").
-			 *sauf que cela ne marche plus pour une utilisation dans une fonction externe à l'objet
-			 *puis que, en prenant l'exemple de "setInterval" rattachée à l'objet "window" 
-			 *le "this" fera référence à l'objet "window" et non à l'objet voulu.
-			 *en ce moment là faudra faire setIinterval( obj.changeIMGLoader, time, obj).
-			*/
-        this.changeIMGLoader = function( loader ){
+         *le paramètre "loader" n'est nécessaire lorsque la fonction est passer en paramètre à une autre. 
+         *en effet il fera le plus souvent référence à l'objet lui même (au "this").
+         *sauf que cela ne marche plus pour une utilisation dans une fonction externe à l'objet
+         *puis que, en prenant l'exemple de "setInterval" rattachée à l'objet "window" 
+         *le "this" fera référence à l'objet "window" et non à l'objet voulu.
+         *en ce moment là faudra faire setIinterval( obj.changeIMGLoader, time, obj).
+         */
+        this.changeIMGLoader = function (loader) {
 
             //ld = loader ou this ( l'objet lui même) 
-            var ld = ( loader instanceof iJS.mi_loader ) ? loader : this ;
+            var ld = (loader instanceof iJS.mi_loader) ? loader : this;
 
-            if( ld.imgDir && ld.imgLength && ld.imgGlobalName)
-                if( ld.imgIndex < ld.imgLength )
-                {
-                    ld.imgPath = ld.imgDir + "/" + ld.imgGlobalName + ld.imgIndex + "." + ld.imgFormat ;
-                    ld.imgIndex++ ;
-                }
-                else
-                {
-                    ld.imgIndex = 0 ;
+            if (ld.imgDir && ld.imgLength && ld.imgGlobalName)
+                if (ld.imgIndex < ld.imgLength) {
+                    ld.imgPath = ld.imgDir + "/" + ld.imgGlobalName + ld.imgIndex + "." + ld.imgFormat;
+                    ld.imgIndex++;
+                } else {
+                    ld.imgIndex = 0;
                 }
 
 
-            if(  ld.imgContainer )
-                ld.imgContainer.src = ld.imgPath ;	
+            if (ld.imgContainer)
+                ld.imgContainer.src = ld.imgPath;
         }
 
         /*permet de lancer l'animation ( par remplacement successif d'image
-			 *suivant une intervalle de temps défini par "timeInterval"
-			 *par défaut l'intervalle de temps est de 150ms
-			*/
-        this.startLoading = function( timeInterval ){
+         *suivant une intervalle de temps défini par "timeInterval"
+         *par défaut l'intervalle de temps est de 150ms
+         */
+        this.startLoading = function (timeInterval) {
 
-            if( this.loaderID )//alors faut d'abord stopper l'animation encours 
-                this.stopLoading() ;
+            if (this.loaderID) //alors faut d'abord stopper l'animation encours 
+                this.stopLoading();
 
-            if( iJS.isNumber( timeInterval ) )
-                this.loaderID = setInterval( this.changeIMGLoader, timeInterval, this )
-                else{
-                    this.loaderID = setInterval( this.changeIMGLoader, 150, this ) ;					
-                }
+            if (iJS.isNumber(timeInterval))
+                this.loaderID = setInterval(this.changeIMGLoader, timeInterval, this)
+            else {
+                this.loaderID = setInterval(this.changeIMGLoader, 150, this);
+            }
 
         }
 
         /*permet de stopper l'animation; soit immédiatement, soit après un un certain temps défini ici par "time"*/
-        this.stopLoading = function( time ){
+        this.stopLoading = function (time) {
 
-            if( iJS.isNumber( time ) )
-            {
-                setTimeout( function( loader ){
-                    if(loader instanceof iJS.mi_loader)
-                    { 
-                        clearInterval( loader.loaderID ); 
-                        loader.imgIndex=0; 
-                        loader.loaderID = 0 ;
-                        loader.changeIMGLoader(); 
+            if (iJS.isNumber(time)) {
+                setTimeout(function (loader) {
+                    if (loader instanceof iJS.mi_loader) {
+                        clearInterval(loader.loaderID);
+                        loader.imgIndex = 0;
+                        loader.loaderID = 0;
+                        loader.changeIMGLoader();
                     }
-                }, time, this 
-                          ) ;
-            }
-            else
-            {
-                clearInterval( this.loaderID ); 
-                this.imgIndex=0; 
-                this.loaderID = 0 ;
-                this.changeIMGLoader() ;
+                }, time, this);
+            } else {
+                clearInterval(this.loaderID);
+                this.imgIndex = 0;
+                this.loaderID = 0;
+                this.changeIMGLoader();
             }
 
         }
     }
 
-} 
+}
